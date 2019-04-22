f8c4355b4e266c8d5d2a4d75cfc903e2da7dc006
Ross Saunders <Ross.Saunders@bhdgsystematic.com>
2019-04-22 22:23:56 +0100

    Adding scripts to setup the development environment

---
+++ SetupEnvironments.ps1
@@ -0,0 +1,20 @@
+$VersionsSupported = @(
+    "7.1.3.14"
+    "7.1.3.19"
+)
+
+foreach($version in $VersionsSupported) {
+
+    $path = Read-Host -Prompt "Enter the path to the $version installation root folder (Leave blank if N/A)"
+
+    if(![string]::IsNullOrWhitespace($path)) {
+
+        if(Test-Path $path) {
+
+            $versionWithoutDots = $version.Replace(".","") 
+            $versionCode = "Sophis_v($versionWithoutDots)_x64"
+        
+            [Environment]::SetEnvironmentVariable($versionCode, $path, "User")
+        }
+    }
+}
---
+++ libs/Finastra/7.1.3.19/CopyRequired.ps1
@@ -0,0 +1,13 @@
+$RequiredFiles = @(
+    "SophisDotNetToolkit.dll"
+    "DevExpress.XtraTreeList.v11.2.dll"
+    "Sophis.Windows.Widgets.dll"
+    "SophisDotNetToolkit.dll"
+    "SophisDotNetToolkitGUI.dll"
+    "SophisDotNetTools.dll"
+    "SophisPortfoliosGUI.dll"
+)
+
+foreach ($file in $RequiredFiles) {
+    Copy-Item -LiteralPath (Join-Path $env:Sophis_v71319_x64 $file) -Destination $PSScriptRoot
+}
