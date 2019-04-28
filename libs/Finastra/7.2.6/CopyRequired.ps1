2bc31f5b6f689727ff5271e97e46d9bf5e112a22
Ross Saunders <ross@rxdsolutions.co.uk>
2019-04-28 21:15:59 +0100

    Updated environment setup scripts

--- libs/Finastra/7.2.6/CopyRequired.ps1
+++ libs/Finastra/7.2.6/CopyRequired.ps1
@@ -1,13 +1,16 @@
 $RequiredFiles = @(
-    "SophisDotNetToolkit.dll"
-    "DevExpress.XtraTreeList.v11.2.dll"
-    "Sophis.Windows.Widgets.dll"
+    "Sophis.DotNetCore.dll"
     "SophisDotNetToolkit.dll"
     "SophisDotNetToolkitGUI.dll"
+    "DevExpress.XtraTreeList.v18.1.dll"
     "SophisDotNetTools.dll"
     "SophisPortfoliosGUI.dll"
+    "Sophis.API.Configuration.dll"
+    "SophisConfiguration.dll"
+    "Sophis.Windows.Widgets.dll"
+    "Sophis.Windows.Widgets.Core.dll"
 )
 
 foreach ($file in $RequiredFiles) {
-    Copy-Item -LiteralPath (Join-Path $env:Sophis_v71319_x64 $file) -Destination $PSScriptRoot
+    Copy-Item -LiteralPath (Join-Path $env:Sophis_v726_x64 $file) -Destination $PSScriptRoot
 }
