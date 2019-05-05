6f53470458d862c9a3d5264eb3143495f6af203f
Ross Saunders <ross@rxdsolutions.co.uk>
2019-05-05 21:40:52 +0100

    Large upgrade to the MFCCaptionBar library

--- src/Controls/MFCCaptionBar10/CaptionBar.cpp
+++ src/Controls/MFCCaptionBar10/CaptionBar.cpp
@@ -1,33 +1,137 @@
 #include "CaptionBar.h"
-#include "framework.h"
+#include <windows.h>
+#include <strsafe.h>
+#include <msclr\marshal.h>
+
+using namespace System;
+using namespace System::Drawing;
+using namespace System::Runtime::InteropServices;
+using namespace msclr::interop;
 
 CaptionBar::CaptionBar()
 {
+	del = gcnew GetMsgProcDelegate(this, &CaptionBar::GetMsgProc);
 }
 
-void CaptionBar::Show()
+LRESULT CALLBACK CaptionBar::GetMsgProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-	static CMFCCaptionBar m_wndCaptionBar;
+	if (nCode < 0) // do not process message 
+		return CallNextHookEx(hook, nCode, wParam, lParam);
 
-	if (!_isCreated)
+	CWPSTRUCT cwp = *(CWPSTRUCT*)lParam;
+	if (cwp.message == WM_COMMAND)
 	{
-		BOOL result = m_wndCaptionBar.Create(WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS, AfxGetMainWnd(), 100000, -1, TRUE);
+		if (cwp.wParam == ID_TOOLS_OPTIONS)
+		{
+			OnButtonClicked(this, gcnew System::EventArgs());
+		}
 	}
 
-	m_wndCaptionBar.SetText(this->Text, CMFCCaptionBar::ALIGN_LEFT);
-	m_wndCaptionBar.ShowWindow(SW_SHOW);
+	return CallNextHookEx(hook, nCode, wParam, lParam);
+}
 
-	dynamic_cast<CFrameWnd*>(AfxGetMainWnd())->RecalcLayout(FALSE);
+static GCHandle gch;
+static CMFCCaptionBar m_wndCaptionBar;
+
+void CaptionBar::Create()
+{
+	//if (!m_wndCaptionBar.Create(WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS, this, ID_VIEW_CAPTION_BAR, -1, TRUE))
+	BOOL result = m_wndCaptionBar.Create(WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS, AfxGetMainWnd(), 100000, -1, TRUE);
+
+	gch = GCHandle::Alloc(del);
+	IntPtr ip = Marshal::GetFunctionPointerForDelegate(del);
+	HOOKPROC hp = static_cast<HOOKPROC>(ip.ToPointer());
+	hook = SetWindowsHookEx(WH_CALLWNDPROC, hp, (HINSTANCE)NULL, GetCurrentThreadId());
 
 	_isCreated = true;
 }
 
+void CaptionBar::Update()
+{
+	if (!_isCreated)
+	{
+		Create();
+	}
+
+	//Create the button
+	if (DisplayButton)
+	{
+		marshal_context context;
+		LPCTSTR cstrT = context.marshal_as<const TCHAR*>(this->ButtonText);
+		LPCTSTR cstrTT = context.marshal_as<const TCHAR*>(this->ButtonToolTip);
+
+		m_wndCaptionBar.SetButton(cstrT, ID_TOOLS_OPTIONS, CMFCCaptionBar::ALIGN_LEFT, FALSE);
+		m_wndCaptionBar.SetButtonToolTip(cstrTT);
+	}
+
+	if (this->Image != nullptr)
+	{
+		HBITMAP hbitMap = (HBITMAP)this->Image->GetHbitmap().ToInt32();
+		m_wndCaptionBar.SetBitmap(hbitMap, RGB(255, 255, 255), FALSE, CMFCCaptionBar::ALIGN_LEFT);
+	}
+
+	m_wndCaptionBar.SetText(this->Text, CMFCCaptionBar::ALIGN_LEFT);
+}
+
+void CaptionBar::Show()
+{
+	if (!_isCreated)
+	{
+		Create();
+	}
+
+	m_wndCaptionBar.ShowWindow(SW_SHOW);
+
+	Update();
+}
+
+Bitmap^ CaptionBar::Image::get()
+{
+	return _image;
+}
+
+void CaptionBar::Image::set(Bitmap^ value)
+{
+	_image = value;
+}
+
 System::String^ CaptionBar::Text::get()
 {
-	return _text;
+	return _messageText;
 }
 
 void CaptionBar::Text::set(System::String^ value)
 {
-	_text = value;
+	_messageText = value;
+	Update();
+}
+
+System::String^ CaptionBar::ButtonText::get()
+{
+	return _buttonText;
+}
+
+void CaptionBar::ButtonText::set(System::String^ value)
+{
+	_buttonText = value;
+}
+
+System::String^ CaptionBar::ButtonToolTip::get()
+{
+	return _buttonToolTip;
+}
+
+void CaptionBar::ButtonToolTip::set(System::String^ value)
+{
+	_buttonToolTip = value;
+}
+
+System::Boolean^ CaptionBar::DisplayButton::get()
+{
+	return _displayButton;
+}
+
+void CaptionBar::DisplayButton::set(System::Boolean^ value)
+{
+	_displayButton = value;
 }
--- src/Controls/MFCCaptionBar12/framework.h
+++ src/Controls/MFCCaptionBar12/framework.h
@@ -33,4 +33,4 @@
 #include <afxcmn.h>                     // MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
-
+#include "Resource/resource.h"       // main symbols
