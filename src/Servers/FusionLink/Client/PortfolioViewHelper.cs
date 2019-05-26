f43e1d844154fae9680ae563745bfe9ac335cf10
Ross Saunders <ross@rxdsolutions.co.uk>
2019-05-26 16:40:30 +0100

    Refactored the Formula builders inside Sophis

---
+++ src/Servers/FusionLink/Client/PortfolioViewHelper.cs
@@ -0,0 +1,37 @@
+﻿//  Copyright (c) RXD Solutions. All rights reserved.
+//  FusionLink is licensed under the MIT license. See LICENSE.txt for details.
+
+using System.Collections.Generic;
+using System.Linq;
+using DevExpress.XtraTreeList.Columns;
+using Sophis.Data.Utils;
+using Sophis.Portfolios;
+
+namespace RxdSolutions.FusionLink
+{
+    public static class PortfolioViewHelper
+    {
+        public static string GetSelectedColumn()
+        {
+            var pv = GetPortfolioView();
+            var column = pv.TreeList.FocusedColumn;
+            return column.Caption;
+        }
+
+        public static IEnumerable<string> GetDisplayedColumns()
+        {
+            var pv = GetPortfolioView();
+            var columns = pv.TreeList.Columns;
+            return columns
+                        .Cast<TreeListColumn>()
+                        .Select(x => x.Caption)
+                        .Where(x => !string.IsNullOrWhiteSpace(x));
+        }
+
+        private static PortfolioView GetPortfolioView()
+        {
+            var pv = NavigationManager.Instance.FocusedTreeView as PortfolioView;
+            return pv;
+        }
+    }
+}
