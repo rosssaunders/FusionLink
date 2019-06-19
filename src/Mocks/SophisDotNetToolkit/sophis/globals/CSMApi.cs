5ee4557b3fce3ec8ce68fca5a654f7ea8623ca1e
Ross Saunders <ross@rxdsolutions.co.uk>
2019-06-19 18:16:04 +0100

    Mocks updated with the new Api calls

---
+++ src/Mocks/SophisDotNetToolkit/sophis/globals/CSMApi.cs
@@ -0,0 +1,15 @@
+﻿//  Copyright (c) RXD Solutions. All rights reserved.
+//  FusionLink is licensed under the MIT license. See LICENSE.txt for details.
+
+using System;
+
+namespace sophis.globals
+{
+    public class CSMApi
+    {
+        public unsafe static global::CSMApi gApi()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
