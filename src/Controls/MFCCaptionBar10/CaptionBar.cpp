c1bc335b573d89e692c71da0734a924da15ab851
Ross Saunders <ross@rxdsolutions.co.uk>
2019-06-29 22:10:28 +0100

    Removed the button from the MFC Captionbar (Just v10 for now)

--- src/Controls/MFCCaptionBar10/CaptionBar.cpp
+++ src/Controls/MFCCaptionBar10/CaptionBar.cpp
@@ -8,60 +8,30 @@ using namespace System::Drawing;
 using namespace System::Runtime::InteropServices;
 using namespace msclr::interop;
 
-CaptionBar::CaptionBar()
-{
-	del = gcnew GetMsgProcDelegate(this, &CaptionBar::GetMsgProc);
-}
-
-LRESULT CALLBACK CaptionBar::GetMsgProc(int nCode, WPARAM wParam, LPARAM lParam)
-{
-	if (nCode < 0) // do not process message 
-		return CallNextHookEx(hook, nCode, wParam, lParam);
-
-	CWPSTRUCT cwp = *(CWPSTRUCT*)lParam;
-	if (cwp.message == WM_COMMAND)
-	{
-		if (cwp.wParam == ID_TOOLS_OPTIONS)
-		{
-			OnButtonClicked(this, gcnew System::EventArgs());
-		}
-	}
-
-	return CallNextHookEx(hook, nCode, wParam, lParam);
-}
-
-static GCHandle gch;
 static CMFCCaptionBar m_wndCaptionBar;
 
-void CaptionBar::Create()
+void RxdSolutions::FusionLink::CaptionBar::Create()
 {
-	//if (!m_wndCaptionBar.Create(WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS, this, ID_VIEW_CAPTION_BAR, -1, TRUE))
 	BOOL result = m_wndCaptionBar.Create(WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS, AfxGetMainWnd(), 100000, -1, TRUE);
-
-	gch = GCHandle::Alloc(del);
-	IntPtr ip = Marshal::GetFunctionPointerForDelegate(del);
-	HOOKPROC hp = static_cast<HOOKPROC>(ip.ToPointer());
-	hook = SetWindowsHookEx(WH_CALLWNDPROC, hp, (HINSTANCE)NULL, GetCurrentThreadId());
 
 	_isCreated = true;
 }
 
-void CaptionBar::Update()
+void RxdSolutions::FusionLink::CaptionBar::Update()
 {
 	if (!_isCreated)
 	{
 		Create();
 	}
 
-	//Create the button
-	if (DisplayButton)
-	{
-		marshal_context context;
-		LPCTSTR cstrT = context.marshal_as<const TCHAR*>(this->ButtonText);
-		LPCTSTR cstrTT = context.marshal_as<const TCHAR*>(this->ButtonToolTip);
+	m_wndCaptionBar.SetText(this->Text, CMFCCaptionBar::ALIGN_LEFT);
+}
 
-		m_wndCaptionBar.SetButton(cstrT, ID_TOOLS_OPTIONS, CMFCCaptionBar::ALIGN_LEFT, FALSE);
-		m_wndCaptionBar.SetButtonToolTip(cstrTT);
+void RxdSolutions::FusionLink::CaptionBar::Show()
+{
+	if (!_isCreated)
+	{
+		Create();
 	}
 
 	if (this->Image != nullptr)
@@ -70,97 +40,32 @@ void CaptionBar::Update()
 		m_wndCaptionBar.SetBitmap(hbitMap, RGB(255, 255, 255), FALSE, CMFCCaptionBar::ALIGN_LEFT);
 	}
 
-	m_wndCaptionBar.SetText(this->Text, CMFCCaptionBar::ALIGN_LEFT);
-}
-
-void CaptionBar::Show()
-{
-	if (!_isCreated)
-	{
-		Create();
-	}
-
 	m_wndCaptionBar.ShowWindow(SW_SHOW);
 
 	Update();
 }
 
-Bitmap^ CaptionBar::Image::get()
+Bitmap^ RxdSolutions::FusionLink::CaptionBar::Image::get()
 {
 	return _image;
 }
 
-void CaptionBar::Image::set(Bitmap^ value)
+void RxdSolutions::FusionLink::CaptionBar::Image::set(Bitmap^ value)
 {
 	_image = value;
 }
 
-System::String^ CaptionBar::Text::get()
+System::String^ RxdSolutions::FusionLink::CaptionBar::Text::get()
 {
 	return _messageText;
 }
 
-void CaptionBar::Text::set(System::String^ value)
+void RxdSolutions::FusionLink::CaptionBar::Text::set(System::String^ value)
 {
 	_messageText = value;
 
 	if (_isCreated)
 	{
 		m_wndCaptionBar.SetText(this->Text, CMFCCaptionBar::ALIGN_LEFT);
-	}
-}
-
-System::String^ CaptionBar::ButtonText::get()
-{
-	return _buttonText;
-}
-
-void CaptionBar::ButtonText::set(System::String^ value)
-{
-	_buttonText = value;
-
-	if (_isCreated)
-	{
-		m_wndCaptionBar.SetText(this->Text, CMFCCaptionBar::ALIGN_LEFT);
-	}
-}
-
-System::String^ CaptionBar::ButtonToolTip::get()
-{
-	return _buttonToolTip;
-}
-
-void CaptionBar::ButtonToolTip::set(System::String^ value)
-{
-	_buttonToolTip = value;
-
-	if (_isCreated && DisplayButton)
-	{
-		marshal_context context;
-		LPCTSTR cstrT = context.marshal_as<const TCHAR*>(this->ButtonText);
-		LPCTSTR cstrTT = context.marshal_as<const TCHAR*>(this->ButtonToolTip);
-
-		m_wndCaptionBar.SetButton(cstrT, ID_TOOLS_OPTIONS, CMFCCaptionBar::ALIGN_LEFT, FALSE);
-		m_wndCaptionBar.SetButtonToolTip(cstrTT);
-	}
-}
-
-System::Boolean^ CaptionBar::DisplayButton::get()
-{
-	return _displayButton;
-}
-
-void CaptionBar::DisplayButton::set(System::Boolean^ value)
-{
-	_displayButton = value;
-
-	if (_isCreated && DisplayButton)
-	{
-		marshal_context context;
-		LPCTSTR cstrT = context.marshal_as<const TCHAR*>(this->ButtonText);
-		LPCTSTR cstrTT = context.marshal_as<const TCHAR*>(this->ButtonToolTip);
-
-		m_wndCaptionBar.SetButton(cstrT, ID_TOOLS_OPTIONS, CMFCCaptionBar::ALIGN_LEFT, FALSE);
-		m_wndCaptionBar.SetButtonToolTip(cstrTT);
 	}
 }
