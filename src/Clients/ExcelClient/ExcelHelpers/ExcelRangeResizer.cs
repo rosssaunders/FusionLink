e9234b1edf9838a524e295731d5813ef3cb71016
Ross Saunders <ross@rxdsolutions.co.uk>
2019-10-19 21:42:44 +0100

    Removed MIT license from the files

--- src/Clients/ExcelClient/ExcelHelpers/ExcelRangeResizer.cs
+++ src/Clients/ExcelClient/ExcelHelpers/ExcelRangeResizer.cs
@@ -1,5 +1,5 @@
 ﻿//  Copyright (c) RXD Solutions. All rights reserved.
-//  FusionLink is licensed under the MIT license. See LICENSE.txt for details.
+
 
 using ExcelDna.Integration;
 using static ExcelDna.Integration.XlCall;
