06e86253c9afdf1343217e8aeadc21f5b2d4288f
Ross Saunders <ross@rxdsolutions.co.uk>
2020-05-01 19:30:45 +0100

    Removed old code

--- src/Servers/FusionLink/Services/InstrumentService.cs
+++ src/Servers/FusionLink/Services/InstrumentService.cs
@@ -3,9 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using RxdSolutions.FusionLink.Model;
-using sophis.inflation;
-using sophis.instrument;
-using sophis.utils;
 
 namespace RxdSolutions.FusionLink.Services
 {
@@ -36,36 +33,5 @@ namespace RxdSolutions.FusionLink.Services
         {
             return _dispatchMapping[typeof(T)][property].Invoke(instrument);
         }
-
-        //public object GetValueX<T>(T instrument, string property) where T : CSMInstrument
-        //{
-        //    return InstrumentFields.GetValue(instrument, property);
-
-        //    switch (instrument)
-        //    {
-        //        case CSMIssuer h: return IssuerFieldLookup.ContainsKey(propertyUpper) ? IssuerFieldLookup[propertyUpper](h) : FB();
-        //        case CSMNonDeliverableForexForward k: return NDFFieldLookup.ContainsKey(propertyUpper) ? NDFFieldLookup[propertyUpper](k) : FB();
-        //        case CSMCapFloor b: return CapFloorFields.ContainsKey(propertyUpper) ? CapFloorFields[propertyUpper](b) : FB();
-        //        case CSMCommission c: return CommissionFields.ContainsKey(propertyUpper) ? CommissionFields[propertyUpper](c) : FB();
-        //        case CSMOption d: return OptionFields.ContainsKey(propertyUpper) ? OptionFields[propertyUpper](d) : FB();
-        //        case CSMForexSpot e: return ForexFieldLookup.ContainsKey(propertyUpper) ? ForexFieldLookup[propertyUpper](e) : FB();
-        //        case CSMFuture f: return FutureFieldLookup.ContainsKey(propertyUpper) ? FutureFieldLookup[propertyUpper](f) : FB();
-        //        case CSMContractForDifference g: return CFDFieldLookup.ContainsKey(propertyUpper) ? CFDFieldLookup[propertyUpper](g) : FB();
-        //        case CSMLoanAndRepo l: return LoanAndRepoFieldLookup.ContainsKey(propertyUpper) ? LoanAndRepoFieldLookup[propertyUpper](l) : FB();
-        //        case CSMBondBasket n: return BondBasketFieldLookup.ContainsKey(propertyUpper) ? BondBasketFieldLookup[propertyUpper](n) : FB();
-        //        case CSMBond o: return BondFieldLookup.ContainsKey(propertyUpper) ? BondFieldLookup[propertyUpper](o) : FB();
-        //        case CSMCommodityBasket u: return CommodityBasketFields.ContainsKey(propertyUpper) ? CommodityBasketFields[propertyUpper](u) : FB();
-        //        case CSMCommodity q: return CommodityFieldLookup.ContainsKey(propertyUpper) ? CommodityFieldLookup[propertyUpper](q) : FB();
-        //        case CSMInterestRate r: return InterestRateFieldLookup.ContainsKey(propertyUpper) ? InterestRateFieldLookup[propertyUpper](r) : FB();
-        //        case CSMSwap s: return SwapFields.ContainsKey(propertyUpper) ? SwapFields[propertyUpper](s) : FB();
-        //        case CSMDebtInstrument t: return DebtInstrumentFields.ContainsKey(propertyUpper) ? DebtInstrumentFields[propertyUpper](t) : FB();
-        //        case CSMForexFuture x: return ForexFutureLookupFields.ContainsKey(propertyUpper) ? ForexFutureLookupFields[propertyUpper](x) : FB();
-        //        case CSMAmFund z: return FundFields.ContainsKey(propertyUpper) ? FundFields[propertyUpper](z) : FB();
-        //        case CSMEquity e: return EquityFields.ContainsKey(propertyUpper) ? EquityFields[propertyUpper](e) : FB();
-        //        case CSMInstrument i: return InstrumentFieldLookup.ContainsKey(propertyUpper) ? InstrumentFieldLookup[propertyUpper](i) : FB();
-
-        //        default: return FB();
-        //    }
-        //}
     }
 }
